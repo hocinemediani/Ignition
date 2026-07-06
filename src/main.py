e314660a219680d207e5784e0868d45021663e04
hocinemediani <dolcejeannarciss@gmail.com>
2026-07-06 14:04:48 +0200

    benchmark of the time per operation of the pipeline. added texts on top of the detectionboxes and made them a different color based on the detected object. transfered normalization and transposition steps to the model instead of the jetson's cpu. buffer requeue now happens earlier to minimize clunkyness from unavailable buffers. fps displaying in the name of the cv2 window.

--- src/main.py
+++ src/main.py
@@ -25,6 +25,22 @@ class CameraInfo(ctypes.Structure):
         ("height", ctypes.c_int)
     ]
 
+# Liste des classes identifiables.
+COCO_CLASSES = [
+    "person", "bicycle", "car", "motorcycle", "airplane", "bus", "train", "truck", "boat", 
+    "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat", 
+    "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", 
+    "umbrella", "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", 
+    "kite", "baseball bat", "baseball glove", "skateboard", "surfboard", "tennis racket", 
+    "bottle", "wine glass", "cup", "fork", "knife", "spoon", "bowl", "banana", "apple", 
+    "sandwich", "orange", "broccoli", "carrot", "hot dog", "pizza", "donut", "cake", 
+    "chair", "couch", "potted plant", "bed", "dining table", "toilet", "tv", "laptop", 
+    "mouse", "remote", "keyboard", "cell phone", "microwave", "oven", "toaster", "sink", 
+    "refrigerator", "book", "clock", "vase", "scissors", "teddy bear", "hair drier", "toothbrush"
+]
+
+CLASS_COLOR = np.random.randint(0, 256, size=(80, 3), dtype=int)
+
 # Configuration des différentes fonctions qui seront appelées.
 
 ## void loadEngine(char *modelPath);
@@ -46,7 +62,10 @@ lib.initializeCamera.restype = None
 lib.getCameraInfo.restype = CameraInfo
 
 # Initialisation de l'environnement.
-lib.initializeCamera(0, 0, 0)
+camWidth = int(input("Quelle est la largeur voulue pour le flux video ? "))
+camHeight = int(input("Quelle est la hauteur voulue pour le flux video ? "))
+camIndex = int(input("Quel est l'index de la caméra voulue ? "))
+lib.initializeCamera(camWidth, camHeight, camIndex)
 
 camInfo = lib.getCameraInfo()
 cameraWidth = camInfo.width
@@ -55,104 +74,117 @@ cameraHeight = camInfo.height
 time.sleep(0.5)
 
 # Optimisation du modèle.
-onnxGraph = graphsurgeon.import_onnx(onnx.load("./models/yolov8n.onnx"))
+if (str(input("Souhaitez vous enregistrer les modifications au modèle ? ")) == "y"):
+    onnxGraph = graphsurgeon.import_onnx(onnx.load("./models/yolov8n.onnx"))
 
-## Transposition des boîtes et des classes.
-transposeOutput = graphsurgeon.Variable("transpose-output", np.float32, (1, 8400, 84))
-transposeNode = graphsurgeon.Node("Transpose", "transpose-node", {"perm": [0, 2, 1]}, [onnxGraph.outputs[0]], [transposeOutput])
-onnxGraph.nodes.append(transposeNode)
+    ## Normalisation de l'input.
+    newInput = graphsurgeon.Variable("new-input", np.float32, (1, 640, 640, 3))
+    twoFiftyFive = graphsurgeon.Constant("two-fifty-five", np.array([255.0], np.float32))
+    normalizedInput = graphsurgeon.Variable("normalized-input", np.float32, (1, 640, 640, 3))
+    divideInputNode = graphsurgeon.Node("Div", "divide-input-node", inputs=[newInput, twoFiftyFive], outputs=[normalizedInput])
+    onnxGraph.nodes.append(divideInputNode)
 
-## Séparation en 2 tenseurs : boîtes et classes.
-boxesOutput = graphsurgeon.Variable("boxes-output", np.float32, (1, 8400, 4))
-classesOutput = graphsurgeon.Variable("classes-output", np.float32, (1, 8400, 80))
+    ## Transposition des canaux.
+    transposeInputNode = graphsurgeon.Node("Transpose", "transpose-input", {"perm": [0, 3, 1, 2]}, [normalizedInput], [onnxGraph.inputs[0]])
+    onnxGraph.nodes.append(transposeInputNode)
+    onnxGraph.inputs = [newInput]
 
-boxesStart = graphsurgeon.Constant("boxes-start", np.array([0], dtype = np.int64))
-classesStart = graphsurgeon.Constant("classes-start", np.array([4], dtype = np.int64))
+    ## Transposition des boîtes et des classes.
+    transposeOutput = graphsurgeon.Variable("transpose-output", np.float32, (1, 8400, 84))
+    transposeNode = graphsurgeon.Node("Transpose", "transpose-node", {"perm": [0, 2, 1]}, [onnxGraph.outputs[0]], [transposeOutput])
+    onnxGraph.nodes.append(transposeNode)
 
-boxesEnd = graphsurgeon.Constant("boxes-end", np.array([4], dtype = np.int64))
-classesEnd = graphsurgeon.Constant("classes-end", np.array([84], dtype = np.int64))
+    ## Séparation en 2 tenseurs : boîtes et classes.
+    boxesOutput = graphsurgeon.Variable("boxes-output", np.float32, (1, 8400, 4))
+    classesOutput = graphsurgeon.Variable("classes-output", np.float32, (1, 8400, 80))
 
-axis = graphsurgeon.Constant("axis", np.array([2], dtype = np.int64))
-step = graphsurgeon.Constant("step", np.array([1], dtype = np.int64))
+    boxesStart = graphsurgeon.Constant("boxes-start", np.array([0], dtype = np.int64))
+    classesStart = graphsurgeon.Constant("classes-start", np.array([4], dtype = np.int64))
 
-boxesSliceNode = graphsurgeon.Node("Slice", "boxes-slice-node", inputs=[transposeOutput, boxesStart, boxesEnd, axis, step], outputs=[boxesOutput])
-classesSliceNode = graphsurgeon.Node("Slice", "classes-slice-node", inputs=[transposeOutput, classesStart, classesEnd, axis, step], outputs=[classesOutput])
-onnxGraph.nodes.append(boxesSliceNode)
-onnxGraph.nodes.append(classesSliceNode)
+    boxesEnd = graphsurgeon.Constant("boxes-end", np.array([4], dtype = np.int64))
+    classesEnd = graphsurgeon.Constant("classes-end", np.array([84], dtype = np.int64))
 
-## Ajout de la couche de Non Maximum Suppression.
+    axis = graphsurgeon.Constant("axis", np.array([2], dtype = np.int64))
+    step = graphsurgeon.Constant("step", np.array([1], dtype = np.int64))
 
-### Conversion de x_centre, y_centre, largeur, hauteur à
-###               x_min, y_min, x_max, y_max
-centerX = graphsurgeon.Variable("center-x", np.float32, (1, 8400, 1))
-centerY = graphsurgeon.Variable("center-y", np.float32, (1, 8400, 1))
-width = graphsurgeon.Variable("width", np.float32, (1, 8400, 1))
-height = graphsurgeon.Variable("height", np.float32, (1, 8400, 1))
+    boxesSliceNode = graphsurgeon.Node("Slice", "boxes-slice-node", inputs=[transposeOutput, boxesStart, boxesEnd, axis, step], outputs=[boxesOutput])
+    classesSliceNode = graphsurgeon.Node("Slice", "classes-slice-node", inputs=[transposeOutput, classesStart, classesEnd, axis, step], outputs=[classesOutput])
+    onnxGraph.nodes.append(boxesSliceNode)
+    onnxGraph.nodes.append(classesSliceNode)
 
-xMin = graphsurgeon.Variable("x-min", np.float32, (1, 8400, 1))
-yMin = graphsurgeon.Variable("y-min", np.float32, (1, 8400, 1))
-xMax = graphsurgeon.Variable("x-max", np.float32, (1, 8400, 1))
-yMax = graphsurgeon.Variable("y-max", np.float32, (1, 8400, 1))
+    ## Ajout de la couche de Non Maximum Suppression.
 
-coordinateSplitNode = graphsurgeon.Node("Split", "coordinate-slice-node", {"axis": 2}, [boxesOutput], [centerX, centerY, width, height])
-onnxGraph.nodes.append(coordinateSplitNode)
+    ### Conversion de x_centre, y_centre, largeur, hauteur à
+    ###               x_min, y_min, x_max, y_max
+    centerX = graphsurgeon.Variable("center-x", np.float32, (1, 8400, 1))
+    centerY = graphsurgeon.Variable("center-y", np.float32, (1, 8400, 1))
+    width = graphsurgeon.Variable("width", np.float32, (1, 8400, 1))
+    height = graphsurgeon.Variable("height", np.float32, (1, 8400, 1))
 
-### x_min = x_centre - (width / 2)
-### y_min = y_centre - (height / 2)
-### x_max = x_centre + (width / 2)
-### y_max = y_centre + (height / 2)
-two = graphsurgeon.Constant("two", np.array([2], np.float32))
-halfHeight = graphsurgeon.Variable("half-height", np.float32, (1, 8400, 1))
-halfWidth = graphsurgeon.Variable("half-width", np.float32, (1, 8400, 1))
+    xMin = graphsurgeon.Variable("x-min", np.float32, (1, 8400, 1))
+    yMin = graphsurgeon.Variable("y-min", np.float32, (1, 8400, 1))
+    xMax = graphsurgeon.Variable("x-max", np.float32, (1, 8400, 1))
+    yMax = graphsurgeon.Variable("y-max", np.float32, (1, 8400, 1))
 
-halfHeightNode = graphsurgeon.Node("Div", "half-height-node", inputs=[height, two], outputs=[halfHeight])
-halfWidthNode = graphsurgeon.Node("Div", "half-width-node", inputs=[width, two], outputs=[halfWidth])
-onnxGraph.nodes.append(halfHeightNode)
-onnxGraph.nodes.append(halfWidthNode)
+    coordinateSplitNode = graphsurgeon.Node("Split", "coordinate-slice-node", {"axis": 2}, [boxesOutput], [centerX, centerY, width, height])
+    onnxGraph.nodes.append(coordinateSplitNode)
 
-xMinNode = graphsurgeon.Node("Sub", "x-min-node", inputs=[centerX, halfWidth], outputs=[xMin])
-yMinNode = graphsurgeon.Node("Sub", "y-min-node", inputs=[centerY, halfHeight], outputs=[yMin])
-xMaxNode = graphsurgeon.Node("Add", "x-max-node", inputs=[centerX, halfWidth], outputs=[xMax])
-yMaxNode = graphsurgeon.Node("Add", "y-max-node", inputs=[centerY, halfHeight], outputs=[yMax])
-onnxGraph.nodes.append(xMinNode)
-onnxGraph.nodes.append(yMinNode)
-onnxGraph.nodes.append(xMaxNode)
-onnxGraph.nodes.append(yMaxNode)
+    ### x_min = x_centre - (width / 2)
+    ### y_min = y_centre - (height / 2)
+    ### x_max = x_centre + (width / 2)
+    ### y_max = y_centre + (height / 2)
+    two = graphsurgeon.Constant("two", np.array([2], np.float32))
+    halfHeight = graphsurgeon.Variable("half-height", np.float32, (1, 8400, 1))
+    halfWidth = graphsurgeon.Variable("half-width", np.float32, (1, 8400, 1))
 
-### Concatenation des nouvelles variables en un seul tenseur.
-correctBoxesOutput = graphsurgeon.Variable("correct-boxes-output", np.float32, (1, 8400, 4))
-concatenateNode = graphsurgeon.Node("Concat", "concatenate-node", {"axis": 2}, [xMin, yMin, xMax, yMax], [correctBoxesOutput])
-onnxGraph.nodes.append(concatenateNode)
+    halfHeightNode = graphsurgeon.Node("Div", "half-height-node", inputs=[height, two], outputs=[halfHeight])
+    halfWidthNode = graphsurgeon.Node("Div", "half-width-node", inputs=[width, two], outputs=[halfWidth])
+    onnxGraph.nodes.append(halfHeightNode)
+    onnxGraph.nodes.append(halfWidthNode)
 
-nmsInput = graphsurgeon.Variable("nms-input", np.float32, (1, 8400, 1, 4))
-unsqueezeAxis = graphsurgeon.Constant("unsqueeze-axis", np.array([2], np.int64))
-unsqueezeNode = graphsurgeon.Node("Unsqueeze", "unsqueeze-node", inputs=[correctBoxesOutput, unsqueezeAxis], outputs=[nmsInput])
-onnxGraph.nodes.append(unsqueezeNode)
+    xMinNode = graphsurgeon.Node("Sub", "x-min-node", inputs=[centerX, halfWidth], outputs=[xMin])
+    yMinNode = graphsurgeon.Node("Sub", "y-min-node", inputs=[centerY, halfHeight], outputs=[yMin])
+    xMaxNode = graphsurgeon.Node("Add", "x-max-node", inputs=[centerX, halfWidth], outputs=[xMax])
+    yMaxNode = graphsurgeon.Node("Add", "y-max-node", inputs=[centerY, halfHeight], outputs=[yMax])
+    onnxGraph.nodes.append(xMinNode)
+    onnxGraph.nodes.append(yMinNode)
+    onnxGraph.nodes.append(xMaxNode)
+    onnxGraph.nodes.append(yMaxNode)
 
-### Résultats finaux du NSM.
-numBoxes = graphsurgeon.Variable("num-boxes", np.int32, (1, 1))
-filteredBoxes = graphsurgeon.Variable("filtered-boxes", np.float32, (1, 25, 4))
-boxesScores = graphsurgeon.Variable("boxes-scores", np.float32, (1, 25))
-boxesClasses = graphsurgeon.Variable("boxes-classes", np.float32, (1, 25))
+    ### Concatenation des nouvelles variables en un seul tenseur.
+    correctBoxesOutput = graphsurgeon.Variable("correct-boxes-output", np.float32, (1, 8400, 4))
+    concatenateNode = graphsurgeon.Node("Concat", "concatenate-node", {"axis": 2}, [xMin, yMin, xMax, yMax], [correctBoxesOutput])
+    onnxGraph.nodes.append(concatenateNode)
 
-nmsNode = graphsurgeon.Node("BatchedNMSDynamic_TRT", "nms-node", {"shareLocation": 1,
-                                                                  "backgroundLabelId": -1,
-                                                                  "numClasses": 80,
-                                                                  "topK": 4096,
-                                                                  "keepTopK": 25,
-                                                                  "scoreThreshold": 0.5,
-                                                                  "iouThreshold": 0.45,
-                                                                  "clipBoxes": 0,
-                                                                  "isNormalized": 0}, [nmsInput, classesOutput], [numBoxes, filteredBoxes, boxesScores, boxesClasses])
-onnxGraph.nodes.append(nmsNode)
+    nmsInput = graphsurgeon.Variable("nms-input", np.float32, (1, 8400, 1, 4))
+    unsqueezeAxis = graphsurgeon.Constant("unsqueeze-axis", np.array([2], np.int64))
+    unsqueezeNode = graphsurgeon.Node("Unsqueeze", "unsqueeze-node", inputs=[correctBoxesOutput, unsqueezeAxis], outputs=[nmsInput])
+    onnxGraph.nodes.append(unsqueezeNode)
 
-### Configuration de la réelle sortie du modèle.
-onnxGraph.outputs = [numBoxes, filteredBoxes, boxesScores, boxesClasses]
-onnxGraph.cleanup()
+    ### Résultats finaux du NSM.
+    numBoxes = graphsurgeon.Variable("num-boxes", np.int32, (1, 1))
+    filteredBoxes = graphsurgeon.Variable("filtered-boxes", np.float32, (1, 25, 4))
+    boxesScores = graphsurgeon.Variable("boxes-scores", np.float32, (1, 25))
+    boxesClasses = graphsurgeon.Variable("boxes-classes", np.float32, (1, 25))
 
-## Exportation du nouveau modèle ONNX.
-newModel = graphsurgeon.export_onnx(onnxGraph)
-onnx.save_model(newModel, "./models/yolov8nNMS.onnx")
+    nmsNode = graphsurgeon.Node("BatchedNMSDynamic_TRT", "nms-node", {"shareLocation": 1,
+                                                                    "backgroundLabelId": -1,
+                                                                    "numClasses": 80,
+                                                                    "topK": 2048,
+                                                                    "keepTopK": 20,
+                                                                    "scoreThreshold": 0.5,
+                                                                    "iouThreshold": 0.3,
+                                                                    "clipBoxes": 0,
+                                                                    "isNormalized": 0}, [nmsInput, classesOutput], [numBoxes, filteredBoxes, boxesScores, boxesClasses])
+    onnxGraph.nodes.append(nmsNode)
+
+    ### Configuration de la réelle sortie du modèle.
+    onnxGraph.outputs = [numBoxes, filteredBoxes, boxesScores, boxesClasses]
+    onnxGraph.cleanup()
+
+    ## Exportation du nouveau modèle ONNX.
+    newModel = graphsurgeon.export_onnx(onnxGraph)
+    onnx.save_model(newModel, "./models/yolov8nNMS.onnx")
 
 # Vérification de l'existence (ou build) de l'engine.
 lib.loadEngine(b"./models/yolov8nNMS.onnx")
@@ -189,9 +221,17 @@ for i in range (0, numTensors):
 windowName = "Flux Jetson Orin Nano"
 cv2.namedWindow(windowName, cv2.WINDOW_NORMAL)
 
+timer1 = 0
+timer2 = 0
+timer3 = 0
+timer4 = 0
+timer5 = 0
+numFrames = 0
+
 # Boucle de fonctionnement.
 while (True):
     # Récupération de l'image.
+    start = time.time()
 
     ## Récupération de la dernière image obtenue.
     message = lib.getLastFrame()
@@ -208,7 +248,12 @@ while (True):
     ## Récupération du tableaux de pixels.
     ArrayType = ctypes.c_uint8 * message.size
     pixelsArray = ctypes.cast(message.start, ctypes.POINTER(ArrayType)).contents
-    rawImage = np.ctypeslib.as_array(pixelsArray)
+
+    ## Copie du pointeur pour pouvoir libérer au plus vite le buffer et éviter les flux clunky.
+    rawImage = np.ctypeslib.as_array(pixelsArray).copy()
+
+    # Relachement du mutex associé au buffer.
+    lib.releaseLastFrame(message.bufferIndex)
 
     # Préparation de l'image.
 
@@ -233,14 +278,8 @@ while (True):
     ## Ajout des black-boxes pour convenir aux dimensions du tenseur d'entrée.
     fullRgbImage = cv2.copyMakeBorder(resizedImage, topPadding, bottomPadding, leftPadding, rightPadding, cv2.BORDER_CONSTANT, (0, 0, 0))
 
-    ## Normalisation de la valeur des pixels.
-    normalizedImage = (fullRgbImage / 255.0).astype(np.float32)
-
-    ## Passage de HWC à CHW.
-    chwImage = np.transpose(normalizedImage, (2, 0, 1))
-
     ## Ajout de la taille du batch pour correspondre aux dimensions du tenseur d'entrée (1, 3, 640, 640).
-    correctImage = np.expand_dims(chwImage, axis=0)
+    correctImage = np.expand_dims(fullRgbImage, axis=0)
 
     ## Aplatissement de l'image en un vecteur 1D.
     correctImage = np.ascontiguousarray(correctImage)
@@ -266,13 +305,16 @@ while (True):
     ## Dessin des boîtes de détection.
     numBoxes = buffers[1].item()
     for i in range (0, numBoxes):
+        classNumber = int(buffers[4][0][i])
+        boxColor = tuple(CLASS_COLOR[classNumber].tolist())
         coordinates = [int(buffers[2][0][i][j]) for j in range(0, 4)]
-        cv2.rectangle(fullRgbImage, (coordinates[0], coordinates[1]), (coordinates[2], coordinates[3]), (255, 0, 0), 5)
+        detectionString = COCO_CLASSES[classNumber] + " : " + str(round(buffers[3][0][i], 3)) + "%"
+        cv2.rectangle(fullRgbImage, (coordinates[0], coordinates[1]), (coordinates[2], coordinates[3]), boxColor, 5)
+        cv2.putText(fullRgbImage, detectionString, (coordinates[0], coordinates[1]), cv2.FONT_HERSHEY_SIMPLEX, 1, boxColor, 2)
 
     ## Affichage de l'image à l'écran.
     cv2.imshow(windowName, cv2.cvtColor(fullRgbImage, cv2.COLOR_RGB2BGR))
-
-    # Relachement du mutex associé au buffer.
-    lib.releaseLastFrame(message.bufferIndex)
+    end = time.time()
+    cv2.setWindowTitle("Flux Jetson Orin Nano", "Flux Jetson Orin Nano, FPS : " + str(int(1 / (end - start))))
 
 cv2.destroyAllWindows()
